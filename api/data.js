a30b7590a52ca189ec7056a309e9d2df2d51063b
Pablo Gonzalez <pablogonmo@gmail.com>
2024-07-26 13:53:50 +0200

    first commit

---
+++ api/data.js
@@ -0,0 +1,21 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+module.exports = async (req, res) => {
+    try {
+        const url = 'http://books.toscrape.com/';
+        const { data } = await axios.get(url);
+        const $ = cheerio.load(data);
+
+        const data = [];
+        $('article.product_pod').each((i, element) => {
+            const title = $(element).find('h3 a').attr('title');
+            const price = $(element).find('p.price_color').text();
+            data.push({ title, price });
+        });
+
+        res.status(200).json(data);
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to scrape data' });
+    }
+};
